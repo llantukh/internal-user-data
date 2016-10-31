36b4aeddf1e99dfacea9267af9b53c8b59308412
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-10-30 20:11:05 -0400

    made it pretty

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,6 @@ import $ from "jquery"
 function pullData () {
   return $.ajax({
   url: "https://randomuser.me/api/?results=12",
-  success: displayData
 
   });
 };
@@ -18,30 +17,30 @@ function displayData(item) {
     var user = item.results[i];
     var renderHTML =
     `
-    <div class = "users">
-      <img class = "photo" src = "${user.picture.large}">
-      <div class = "name">
-        <p>${user.name.first } ${user.name.last}</p>
+      <div class = "users">
+        <img class = "photo" src = "${user.picture.large}">
+          <div class = "name">
+            <p>${user.name.first } ${user.name.last}</p>
+          </div>
+          <div class = "email">
+            <p>${user.email}</p>
+          </div>
+          <div class = "address">
+            <p>${user.location.street}</p>
+            <p>${user.location.city} ${user.location.state }
+            ${user.location.postcode}</p>
+          </div>
+          <div class = "phone">
+            <p>${user.cell}</p>
+          </div>
+          <div class = "SSN">
+            <p>${user.id.value}</p>
+          </div>
       </div>
-      <div class = "email">
-        <p>${user.email}</p>
-      </div>
-      <div class = "address">
-        <p>${user.location.street}</p>
-        <p>${user.location.city} ${user.location.state }
-        ${user.location.postcode}</p>
-      </div>
-      <div class = "phone">
-        <p>${user.cell}</p>
-      </div>
-      <div class = "SSN">
-        <p>${user.id.value}</p>
-      </div>
-    </div>
 
     `
     $(".container").append(renderHTML);
   };
 };
 
-pullData();
+pullData().then(displayData);
